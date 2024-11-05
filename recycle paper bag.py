ce2db0e65ecde36086f0f84eb799a91b88691d7d
JonEdCode <monika_gouws@yahoo.co.uk>
2024-11-05 17:03:06 +0000

    Add files via upload

---
+++ recycle paper bag.py
@@ -0,0 +1,16 @@
+import pgzrun
+import random
+WIDTH = 1000
+HEIGHT = 600
+TITLE = "recycle_paper_bag " 
+def draw():
+    screen.blit("eco friendly bg",(0,0))
+def update():
+    pass
+
+
+
+
+
+
+pgzrun.go()
